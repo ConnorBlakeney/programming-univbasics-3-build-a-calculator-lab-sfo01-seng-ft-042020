ea1ce3452fa1019e59e8a8b50a8b66713cb6f32b
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-18 02:26:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  diff
 end
 
 def division(num1, num2)
-
+  quotient = num1/ num2
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  remain = num1 % num2
+  remain
 end
 
 def square_root(num)
-
+  
 end
